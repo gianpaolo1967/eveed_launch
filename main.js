18e0a8175ec41a8b78e551d05674155d82f42d5e
gianpaolo1967 <gianpaolo1967@gmail.com>
2018-01-11 15:37:08 +0200

    geo first

--- main.js
+++ main.js
@@ -12,11 +12,11 @@ document.getElementById('btn').onclick = function() {
 		var country = document.getElementById('country').value;
         var post = document.getElementById('post');
 		country = country.toUpperCase();
-		p0.innerHTML = '';
-		p1.innerHTML = '';
-		p2.innerHTML = '';
-		p3.innerHTML = '';
-		p4.innerHTML = '';
+		p0.innerHTML = country;
+		p1.innerHTML = country;
+		p2.innerHTML = country;
+		p3.innerHTML = country;
+		p4.innerHTML = country;
 		for (var i = 0; i < arr.length; i++) {
 			if (i % 2 == 0) {
 				p0.innerHTML += arr[i].charAt(0).toUpperCase();
@@ -35,11 +35,11 @@ document.getElementById('btn').onclick = function() {
 					console.log(arr[i]);
             
 		}
-		p0.innerHTML += '18' + os.charAt(0).toLowerCase() + sex.charAt(0).toLowerCase() + country;
-		p1.innerHTML += '24' + os.charAt(0).toLowerCase() + sex.charAt(0).toLowerCase() + country;
-		p2.innerHTML += '30' + os.charAt(0).toLowerCase() + sex.charAt(0).toLowerCase() + country;
-		p3.innerHTML += '34' + os.charAt(0).toLowerCase() + sex.charAt(0).toLowerCase() + country;
-		p4.innerHTML += '35' + os.charAt(0).toLowerCase() + sex.charAt(0).toLowerCase() + country;
+		p0.innerHTML += '18' + os.charAt(0).toLowerCase() + sex.charAt(0).toLowerCase();
+		p1.innerHTML += '24' + os.charAt(0).toLowerCase() + sex.charAt(0).toLowerCase();
+		p2.innerHTML += '30' + os.charAt(0).toLowerCase() + sex.charAt(0).toLowerCase();
+		p3.innerHTML += '34' + os.charAt(0).toLowerCase() + sex.charAt(0).toLowerCase();
+		p4.innerHTML += '35' + os.charAt(0).toLowerCase() + sex.charAt(0).toLowerCase();
     
         post.innerHTML = 'Post: "' + arr[0] + ' ' + arr[1] + ' ' + arr[2] + ' ' + arr[3] + ' ' + arr[4] + '..." ' + country + '_' + 18 + '<br>' + 'Post: "' + arr[0] + ' ' + arr[1] + ' ' + arr[2] + ' ' + arr[3] + ' ' + arr[4] + '..." ' + country + '_' + 24 + '<br>' + 'Post: "' + arr[0] + ' ' + arr[1] + ' ' + arr[2] + ' ' + arr[3] + ' ' + arr[4] + '..." ' + country + '_' + 30 + '<br>' + 'Post: "' + arr[0] + ' ' + arr[1] + ' ' + arr[2] + ' ' + arr[3] + ' ' + arr[4] + '..." ' + country + '_' + 34 + '<br>'; 
 	
